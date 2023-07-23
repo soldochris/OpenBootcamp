0757574d8c3745ef8a086d8ed2897309c596656d
Christian Soldevilla <soldevilla.christian@gmail.com>
2023-07-22 19:20:53 -0500

    part 1 - basics

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import Message from './Message.js'
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Message color="red" msg="Estamos Trabajando" />
+      <Message color="green" msg="En un curso" />
+      <Message color="blue" msg="De React" />
     </div>
   );
 }
---
+++ part1/src/Message.js
@@ -0,0 +1,8 @@
+function Message(props){
+  return (
+    <h1 style={{color: props.color}}>
+      {props.msg}
+    </h1>)
+}
+
+export default Message
