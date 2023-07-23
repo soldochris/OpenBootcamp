d347b1a8564223ef4fb5c46f09b38f956ed71752
Christian Soldevilla <soldevilla.christian@gmail.com>
2023-07-23 15:22:41 -0500

    part 1 - exercises of JavaScript

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,38 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-/** short form
+/** short form to create the component Header
  *  const Header = ({course}) => <h1>{course}</h1>
  */
-const Header = (props) =>{
-  return <h1>{props.course}</h1>
-}
-
-const Part = (props) =>{
-  return <p>{props.part}: {props.exercises}</p>
-}
-
-const Content = (props) =>{
-  return (
-    <div>
-      <Part part="Fundamentals of React" exercises="10"/>
-      <Part part="Using props to pass data" exercises="7"/>
-      <Part part="State of a component" exercises="14"/>
-    </div>
-  )
-}
-
-const Total = (props) =>{
-  return <p>Number of exercises: {props.total}</p>
-}
 
 const App = () => {
+  const Course = "Half Stack application develpment";
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
-      <Header course="Half Stack application development" />
-      <Content />
-      <Total total="37" />
+      <h1>{Course}</h1>
+      <p>{parts[0].name}: {parts[0].exercises}</p>
+      <p>{parts[1].name}: {parts[1].exercises}</p>
+      <p>{parts[2].name}: {parts[2].exercises}</p>
+      <p>Total: {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>
     </div>
   )
 }
