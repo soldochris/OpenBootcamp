5754af808ed1fa186a73c491dde8657868c6d669
Christian Soldevilla <soldevilla.christian@gmail.com>
2023-07-25 21:43:06 -0500

    example of update sstate with functions

--- part0/pruebas/src/index.js
+++ part0/pruebas/src/index.js
@@ -5,18 +5,35 @@ import { useState } from 'react';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 const App = (props) => {
-  const [counterValue, updateCounter] = useState(1);
+  const [counterValue, updateCounter] = useState(0);
 
-  const handleClick = () => {
-    updateCounter(counterValue + 1);
-  }
+  // const handleClickIncrease = () => {
+  //   updateCounter(counterValue + 1);
+  // };
+  // const handleClickDecrease = () => {
+  //   updateCounter(counterValue - 1);
+  // };
+  // const handleClickReset = () => {
+  //   updateCounter(0);
+  // };
+
+
+  const isEven = counterValue % 2 === 0;
+  const message = isEven ? "is even" : "is odd";
 
   return (
     <div>
       <h1>{counterValue}</h1>
-      <button onClick={handleClick}>
+      <p>{message}</p> 
+      <button onClick={()=>{updateCounter(counterValue + 1)}}>
         Increase
-        </button>
+      </button>
+      <button onClick={()=>{updateCounter(counterValue - 1)}}>
+        Decrease
+      </button>
+      <button onClick={()=>{updateCounter(0)}}>
+        Reset
+      </button>
     </div>
   )
 }
