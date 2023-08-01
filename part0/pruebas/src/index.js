ec68eb514725f0442d5ad8ff22872185dac0dd5b
Christian Soldevilla <soldevilla.christian@gmail.com>
2023-08-01 16:19:32 -0500

    the right way to reset the state

--- part0/pruebas/src/index.js
+++ part0/pruebas/src/index.js
@@ -5,31 +5,47 @@ import "./styles.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
+const WarningNotUsed = () => {
+  return <h1>The counter is not already used!</h1>
+};
+
+const ListOfClicks = ({clicks})=>{
+  return <p>{clicks.join(", ")}</p>
+};
+
+const INITIAL_COUNTER_STATE = {
+  left: 0,
+  right: 0,
+  message: 'This is a message inside the state.'
+}
+
 const App = () => {
   // const [left, setLeft] = useState(0);
   // const [right,setRight] = useState(0);
 
-  const [counters,setCounters] = useState({
-    left: 0,
-    right: 0,
-    clicks: 0,
-    message: 'This is a message inside the state.'
-  });
+  const [counters,setCounters] = useState(INITIAL_COUNTER_STATE);
+
+  const [clicks,setClicks] = useState([]);
 
   const handleLeft = () =>{
     setCounters({
       ...counters,
-      left: counters.left + 1,
-      clicks: counters.clicks + 1
+      left: counters.left + 1
     });
+    setClicks(prevClicks => ([...prevClicks,'L']));
   };
 
   const handleRight = () => {
     setCounters({
       ...counters,
-      right: counters.right + 1,
-      clicks: counters.clicks + 1
+      right: counters.right + 1
     });
+    setClicks(prevClicks => ([...prevClicks,'R']));
+  };
+
+  const handleReset = () =>{
+    setCounters(INITIAL_COUNTER_STATE);
+    setClicks([]);
   };
 
   return (
@@ -42,8 +58,11 @@ const App = () => {
       <button onClick={handleRight}>
         right
       </button>
-      <p>Total number of clicks: {counters.clicks}</p>
+      <br/>
+      <button onClick={handleReset}>Reset</button>
+      <p>Total number of clicks: {clicks.length}</p>
       <p>{counters.message}</p>
+      {clicks.length === 0 ? <WarningNotUsed/> : <ListOfClicks clicks={clicks}/>}
     </div>
   )
 }
