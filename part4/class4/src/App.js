396c688a21efb6451ebff9d499aa0c65bf1ce8a5
Christian Soldevilla <soldevilla.christian@gmail.com>
2023-08-04 22:06:54 -0500

    practicing with key and map()

--- part4/class4/src/App.js
+++ part4/class4/src/App.js
@@ -1,56 +1,59 @@
 import './App.css';
+import { Course }  from './Course.js';
 
-const notes = [
-  {
-    id: 1,
-    content: 'HTML is easy',
-    date: '2019-05-30T17:30:31.098Z',
-    important: true,
-  },
-  {
-    id: 2,
-    content: 'Browser can execute only JavaScript',
-    date: '2019-05-30T18:39:34.091Z',
-    important: false,
-  },
-  {
-    id: 3,
-    content: 'GET and POST are the most important methods of HTTP protocol',
-    date: '2019-05-30T19:20:14.298Z',
-    important: true,
-  },
-]
+const App = () => {
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1,
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2,
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3,
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4,
+        },
+      ],
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1,
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2,
+        },
+      ],
+    },
+  ]
 
-const Note = ({id, content, date}) => {
   return (
-    <div>
-    <strong>{id}</strong>
-    <p>{content}</p>
-    <small><time>{date}</time></small>
-  </div>
-  )
-}
-
-
-function App() {
-  if (typeof notes === 'undefined' || notes.length === 0 || notes === null){
-    return (
-      <div>
-        <h1>No notes</h1>
-      </div>
+    <ul>
+      {courses.map((course) => {
+        return <li key={course.id}><Course course={course}/></li>
+      })}
+    </ul>
     )
-  }
-
-  return (
-    <div>
-      <h1>Hi to class 4</h1>
-      <div>
-        {notes.map( (note) => (
-          <Note key={note.id} content={note.content} date={note.date}/>
-        ))}
-      </div>
-    </div>
-  );
 }
 
 export default App;
