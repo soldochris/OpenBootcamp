fb730c70ca903e470229940e574f7bb35cbe8298
Christian Soldevilla <soldevilla.christian@gmail.com>
2023-08-18 14:41:22 -0500

    api deployed on heroku

--- part4/class4/src/services/notes/createNotes.js
+++ part4/class4/src/services/notes/createNotes.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export const createNote = ({title, body}) => {
-  return axios.post('http://localhost:3001/api/notes/', {title, body} )
+  return axios.post('https://sheltered-hamlet-57841-9cceac2993ca.herokuapp.com/api/notes', {title, body} )
   .then( response => {
     const {data} = response;
     return data;
--- part4/class4/src/services/notes/getAllNotes.js
+++ part4/class4/src/services/notes/getAllNotes.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const getAllNotes = () => {
-  return axios.get('http://localhost:3001/api/notes/')
+  return axios.get('https://sheltered-hamlet-57841-9cceac2993ca.herokuapp.com/api/notes')
   .then((response) => {
     const {data} = response;
     return data;
