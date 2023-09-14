a296ab8313a4de12b61916f2d660998ad7295902
Christian Soldevilla <soldevilla.christian@gmail.com>
2023-09-13 20:16:46 -0500

    serving static files

--- part3/index.js
+++ part3/index.js
@@ -13,25 +13,21 @@ const { ProfilingIntegration } = require("@sentry/profiling-node");
 app.use(cors());
 app.use(express.json());
 
+app.use("/images", express.static("images"));
 
 Sentry.init({
   dsn: "https://a3ea0420c744fcf55ed7f2c981db2e57@o4505875725221888.ingest.sentry.io/4505875732692992",
   integrations: [
-    // enable HTTP calls tracing
     new Sentry.Integrations.Http({ tracing: true }),
-    // enable Express.js middleware tracing
     new Sentry.Integrations.Express({ app }),
     new ProfilingIntegration(),
   ],
-  // Performance Monitoring
-  tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
-  // Set sampling rate for profiling - this is relative to tracesSampleRate
-  profilesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
+  tracesSampleRate: 1.0,
+  profilesSampleRate: 1.0, 
 });
 
 app.use(Sentry.Handlers.requestHandler());
 
-// TracingHandler creates a trace for every incoming request
 app.use(Sentry.Handlers.tracingHandler());
 
 app.get("/", (request, response) => {
